196b0030d68296ea5c357f2da648b88f7a28b1fe
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-06-17 10:19:26 +0800

    Completed give_me_diamond.py

--- codewars/give_me_diamond.py
+++ codewars/give_me_diamond.py
@@ -2,27 +2,23 @@
 Give Me Diamond question from codewars
 """
 def diamond(n):
-# Make some diamonds!
+	# Make some diamonds!
+	if not n: return None
 	if n % 2 == 0: return None
 	if n == 1: return '*'
-
 	output = ''
-	spaces = int(n / 2)
-	counter = 1
-
-	output += (' ' * spaces) + '*\n'
-	star_count = counter
+	no_of_spaces = int(n / 2)
+	space = ' '
+	
+	counter = 0
 	while counter < n:
-		if (counter + 2) <= n:
-			spaces -= 1
-			star_count += 2
-			output += (' ' * spaces) + ('*' * star_count) + '\n'
+		if (counter * 2 + 1) <= n:
+			output += (space * no_of_spaces) + ('*' * (n -  (2 *no_of_spaces))) + '\n'
+			if no_of_spaces > 0: no_of_spaces -= 1 
 		else:
-			spaces += 1
-			star_count -= 2
-			output += (' ' * spaces) + ('*' * star_count) + '\n'
-
+			no_of_spaces += 1
+			output += (space * no_of_spaces) + ('*' * (n -  (2 *no_of_spaces))) + '\n'
 		counter += 1
 	return output
 
-print(diamond(3))
+print(diamond(''))
